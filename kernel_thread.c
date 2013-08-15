5ea9f9628841fb2f7a4a0e82a2138276284f9330
coperdli <lhcwhu@gmail.com>
2013-08-15 21:20:11 +0800

    begin kernel module programming

---
+++ kernel_thread.c
@@ -0,0 +1,67 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/time.h>
+#include <linux/errno.h>
+#include <linux/sched.h>
+#include <linux/kthread.h>
+#include <linux/cpumask.h>
+
+#define sleep_millisecs 1000 * 60
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("coperd <lhcwhu@gmail.com>");
+MODULE_VERSION("v0.1");
+MODULE_DESCRIPTION("kernel thread example");
+MODULE_ALIAS("kthread");
+
+int mythread(void *arg)
+{
+    //long ns1, ns2, delta;
+    unsigned int cpu;
+    //struct timespec ts1, ts2;
+
+    cpu = *((unsigned int *)arg);
+    printk(KERN_INFO "### [thread/%d] test start \n", cpu);
+    while (!kthread_should_stop()) {
+        schedule_timeout_interruptible(msecs_to_jiffies(1));
+    }
+    printk(KERN_INFO "### [thread/%d] test end \n", cpu);
+    return 0;
+}
+
+static int __init kthread_init()
+{
+    int cpu;
+    unsigned int cpu_count = num_online_cpus();
+    unsigned int parameter[cpu_count];
+    struct task_struct *t_thread[cpu_count];
+
+    for_each_present_cpu(cpu) {
+        parameter[cpu] = cpu;
+        
+        t_thread[cpu] = kthread_create(mythread, (void *)(parameter + cpu), "thread/%d", cpu);
+
+        if (IS_ERR(t_thread[cpu])) {
+            printk(KERN_ERR "[thread/%d]: creating kthread failed \n", cpu);
+            goto out;
+        }
+        kthread_bind(t_thread[cpu], cpu);
+        wake_up_process(t_thread[cpu]);
+    }
+    schedule_timeout_interruptible(msecs_to_jiffies(sleep_millisecs));
+    
+    for (cpu = 0; cpu < cpu_count; cpu++) {
+        kthread_stop(t_thread[cpu]);
+    }
+out:
+    return 0;
+}
+
+static void __exit kthread_exit()
+{
+    printk(KERN_INFO "kthread modules unloaded\n");
+}
+
+module_init(kthread_init);
+module_exit(kthread_exit);
---
+++ kthread.c
@@ -0,0 +1,66 @@
+/*-----------------------------------------------------------------------------
+ * Example for the kernel thread API
+ *  - create a simple thread when the module is loaded
+ *  - stop the thread when the modules is unloaded
+ *
+ * file: kthread0.c
+ *
+ * coperdli, Aug/2013
+ *-----------------------------------------------------------------------------*/
+
+
+#include <linux/module.h>	/* Specifically, a module */
+#include <linux/kernel.h>	/* We're doing kernel work */
+#include <linux/init.h>         /* modules_init/_exit macros */
+#include <linux/kthread.h>	/* kthread_run*/
+#include <linux/sched.h>	/* task_struct*/
+#include <linux/delay.h>	/* mdelay()*/
+/*
+ * debug macro
+ */
+#define DEBUG
+
+#ifdef DEBUG
+#define DPRINT(fmt,args...) printk(KERN_INFO "%s,%i:" fmt "\n", \
+                            __FUNCTION__, __LINE__,##args);
+#else
+#define DPRINT(fmt,args...)
+#endif
+
+MODULE_LICENSE("GPL v2");
+/*
+ * the kernel thread
+ */
+int kthread_fct(void *data)
+{
+	while(1) {
+		DPRINT("kernel thread");
+		//msleep(500);
+		mdelay(500);
+		msleep(1);
+		if (kthread_should_stop())
+			break;
+	}
+	return 0;
+}
+
+struct task_struct *ts;
+
+/* 
+ * module init/exit functions
+ */
+int __init kthr_init(void)
+{
+	DPRINT("init_module() called\n");
+	ts=kthread_run(kthread_fct,NULL,"kthread");
+	return 0;
+}
+
+void __exit kthr_exit(void)
+{
+	DPRINT("exit_module() called\n");
+	kthread_stop(ts);
+}
+
+module_init(kthr_init);
+module_exit(kthr_exit);
